9e9a052d28cec54d627c053cc13120fe2b434844
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-02 20:24:13 -0500

    Cleanup

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -131,8 +131,30 @@ public class Graph {
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader file = new BufferedReader(new FileReader(args[0]));
-			
-		Graph G = new Graph(file);
+		
+		Graph G;
+//		G = new Graph(file);
+		G = manualEntry();
+		
 		System.out.println(G);
+	}
+
+	private static Graph manualEntry() {
+		Graph G = new Graph(13);
+		G.addEdge(0, 5);
+		G.addEdge(4, 3);
+		G.addEdge(0, 1);
+		G.addEdge(9, 12);
+		G.addEdge(6, 4);
+		G.addEdge(5, 4);
+		G.addEdge(0, 2);
+		G.addEdge(11, 12);
+		G.addEdge(9, 10);
+		G.addEdge(0, 6);
+		G.addEdge(7, 8);
+		G.addEdge(9, 11);
+		G.addEdge(5, 3);
+		
+		return G;
 	}
 }
